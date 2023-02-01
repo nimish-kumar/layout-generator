869ae320e258b26fd20e8e8873cc8861fef18cd0
Nimish Kumar <nimishk642@gmail.com>
2023-02-01 22:21:45 +0530

    Stepper added to main screen

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,35 @@
-import React from 'react';
+import React, { ReactNode, useState } from 'react';
 import './App.css';
 import 'antd/dist/reset.css';
 import LayoutGenerator from './components/LayoutGeneratorForm';
 import Layout from './components/Layout';
-import { Row } from 'antd';
+import { Col, Row, Typography } from 'antd';
+import Stepper from './components/Stepper';
 
 function App() {
+  const { Title } = Typography;
+  const [activeStep, setStep] = useState(0);
+  const [layout, setLayout] = useState('');
+  const nextStep = () => {
+    setStep((step) => step + 1);
+  };
+  const steps = [
+    {
+      title: 'Layout form',
+      content: <LayoutGenerator setLayout={setLayout} nextStep={nextStep} />,
+    },
+    { title: 'Design layout', content: <Layout layout={layout} setLayout={setLayout} /> },
+  ];
   return (
     <div className='App'>
-      <LayoutGenerator />
-      <Row>
-        <Layout />
+      <Row gutter={[8, 2]} justify='center'>
+        <Col span={24}>
+          <Title level={3}>Layout generator form</Title>
+        </Col>
+        <Col span={2}>
+          <Stepper activeStep={activeStep} steps={steps} />
+        </Col>
+        <Col span={16}>{steps[activeStep].content}</Col>
       </Row>
     </div>
   );
